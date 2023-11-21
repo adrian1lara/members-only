4fca45536207f72e48016d877d45adc0a312ac9f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-21 17:51:53 -0500

    added: Schema model for user and virtual fullname

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  membership: { type: String, required: true }
+})
+
+UserSchema.virtual('fullname').get(function() {
+  return `${this.name} ${this.lastname}`
+})
+
+module.exports = mongoose.model('User', UserSchema)
