bdd4e70793b9cda5f064d94a76fb1c3a89787e20
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-21 18:40:40 -0500

    added: password

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
   membership: { type: String, required: true }
 })
 
