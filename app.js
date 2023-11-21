0d75bb0e2a73a91abe76dc72a95641d03ae70b39
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-21 18:17:40 -0500

    added: dotenv and mongoose

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
