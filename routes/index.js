b00cad360c70d0db84f0caef1318ada616f25cde
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-21 20:56:38 -0500

    added: routes for sign-up form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../controllers/userController')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Get sign-up page
+router.get("/sign-up", (req, res) => res.render("sign-up-form"))
+
+// handle post request sign-up
+router.post("/sign-up", userController.create_user_post)
 
 module.exports = router;
