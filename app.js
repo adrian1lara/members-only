b46a278b1895f93ecd746d80b1322dbf1fc9f4a5
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-22 00:01:09 -0500

    added: passport validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const flash = require('express-flash');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,6 +9,7 @@ const dotenv = require('dotenv')
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/user')
 
 dotenv.config()
 
@@ -41,8 +43,50 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: secretKey, resave: false, saveUninitialized: true }))
+app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
+
+
+passport.use(
+  new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+
+    async (username, password, done) => {
+
+
+      try {
+        const user = await User.findOne({ email: username })
+
+        if (!user) {
+          return done(null, false, { message: "Incorrect email" })
+        }
+
+        if (user.password !== password) {
+          return done(null, false, { message: "Incorrect password" })
+        }
+
+        return done(null, user)
+      } catch (err) {
+        return done(err)
+      }
+    })
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id)
+    done(null, user)
+  } catch (error) {
+    done(error)
+  }
+})
 
 app.use(logger('dev'));
 app.use(express.json());
