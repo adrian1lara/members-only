2f2355074fcb3379a13dd6b74bffa89ca8600de5
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-22 00:04:20 -0500

    added: handle post login method

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const passport = require('passport')
 const router = express.Router();
 
-const userController = require('../controllers/userController')
+const userController = require('../controllers/usercontroller')
 
-/* GET home page. */
+/* get home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'express' });
 });
 
-// Get sign-up page
+// get sign-up page
 router.get("/sign-up", (req, res) => res.render("sign-up-form"))
 
 // handle post request sign-up
 router.post("/sign-up", userController.create_user_post)
+
+
+// get log in page
+router.get("/log-in", (req, res) => res.render("log-in-form"))
+
+
+//handle post log-in
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+    failureFlash: true,
+  })
+);
+
+
 
 module.exports = router;
