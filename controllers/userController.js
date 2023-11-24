fa65c2ddf1b3927a0f6d00a71117ddb398a9a899
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-23 22:20:32 -0500

    added: handle get and post to become a member

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+
 const asyncHandler = require('express-async-handler')
 const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
@@ -66,3 +68,46 @@ exports.create_user_post = [
 ]
 
 
+//get member-form 
+exports.become_member_get = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id).exec()
+
+  if (req.user === null) {
+    const err = new Error('User not found')
+    return next(err)
+  }
+
+  if (req.user) {
+    res.render('member-form', {
+      user: user,
+      error: ''
+    })
+
+  } else {
+    res.redirect('/log-in')
+  }
+
+})
+
+
+
+// handle become a member
+exports.become_member_post = [
+
+  body('password').trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const secretpass = process.env.SECRET_KEY;
+    const user = await User.findById(req.params.id).exec();
+
+    if (req.body.password === secretpass) {
+      await User.findByIdAndUpdate(req.params.id, { membership: 'member' });
+      res.redirect('/');
+    } else {
+      res.render('member-form', {
+        user: user,
+        error: 'Incorrect secret password'
+      });
+    }
+  })
+];
